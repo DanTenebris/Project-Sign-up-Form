225f1edc101238ed9eb242f2d897d6497ede2945
DanTenebris <dantenebris@outlook.com>
2022-04-07 15:29:13 -0400

    All project requirements have been met.

---
+++ README.md
@@ -0,0 +1,8 @@
+# The Odin Project
+## Project: SIGN-UP FORM.
+This is a sign-up form from The Odin Project [curriculum](https://www.theodinproject.com/lessons/node-path-intermediate-html-and-css-sign-up-form).
+### [Live preview](https://dantenebris.github.io/Project-Sign-up-Form/).
+## Outcome of this project:
+- Improved understanding of the DOM, its manipulation, and form validations.
+- Increased JS understanding.
+- Enhanced CSS knowledge and application.
--- sign-upForm.js
+++ sign-upForm.js
@@ -1,34 +1,173 @@
-const loginLink = document.querySelector('.haveAccount > a');
 
-const credit = document.querySelector('.credit');
-const main = document.querySelector('main');
+const form = document.querySelector('form');
+const pwd = document.querySelector('#pwd');
+const pwd2 = document.querySelector('#pwd2');
 
-const creditStyle = getComputedStyle(credit);
-const color1 = creditStyle.color;
+/* console.log(email.matches(':hover')); */
 
-const hasVScroll = main.scrollHeight > (window.innerHeight || document.documentElement.clientHeight);
 
-/* if (hasVScroll) {
-  credit.style.position = 'absolute';
-} else {
-  credit.style.position = 'fixed';
-} */
+/* Prueba seleccionando todos para un add event listener */
+const inputs = document.querySelectorAll('.input');
+inputs.forEach((fillInput) => {
+  const inputError = fillInput.nextElementSibling;
+  fillInput.addEventListener('input', () => {
+    inputValidation(fillInput, inputError);
+  });
 
-/* const isElementXPercentInViewport = function(el, percentVisible) {
-  let
-    rect = el.getBoundingClientRect(),
-    windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+  fillInput.addEventListener('focusin', () => {
+    showAndHideErrorMessage(fillInput, true);
+  });
 
-  return !(
-    Math.floor(100 - (((rect.top >= 0 ? 0 : rect.top) / +-rect.height) * 100)) < percentVisible ||
-    Math.floor(100 - ((rect.bottom - windowHeight) / rect.height) * 100) < percentVisible
-  )
-}; */
+  fillInput.addEventListener('focusout', () => {
+    showAndHideErrorMessage(fillInput, false);
+  });
+});
 
-console.log(credit);
-console.log(color1);
-/* console.log(isElementXPercentInViewport(loginLink, 100)); */
-/* console.log(loginLink.getBoundingClientRect()); */
-console.log(main.scrollHeight);
-console.log(hasVScroll);
-console.log(document.body.scrollHeight);
+function inputValidation(inputValue, inputError) {
+  const spanRequired = inputValue.previousElementSibling.firstElementChild.firstElementChild;
+  const pattern = /^\+([1-9]{3}(\-|\s)?){2}([1-9]{3})$/;
+
+  /* console.log(inputValue.value);
+  console.log(pattern.test(inputValue.value)); */
+
+  if ((inputValue.id === 'pwd2') && (pwd.value !== pwd2.value)) {
+    if (spanRequired) {
+      spanRequired.textContent = '*'
+    }
+    showPasswordConfirmationError(inputValue, inputValue.nextElementSibling);
+    showAndHideErrorMessage(inputValue, true);
+  } else if ((inputValue.id === 'phone') && !(pattern.test(inputValue.value))) {
+    showError(inputValue, inputError);
+    showAndHideErrorMessage(inputValue, true);
+
+  } else if (inputValue.validity.valid) {
+    if (spanRequired) {
+      spanRequired.style.color = 'blue';
+    }
+
+    inputError.textContent = '';
+    showAndHideErrorMessage(inputValue);
+    inputError.className = 'error';
+  } else {
+    if (spanRequired) {
+      spanRequired.style.color = 'red'
+    }
+    showError(inputValue, inputError);
+    showAndHideErrorMessage(inputValue, true);
+  }
+}
+
+function showAndHideErrorMessage(inputValue = null, flag = false) {
+  const errorActive = document.querySelector(`#${inputValue.id} + .error.active`);
+  if (errorActive) {
+    if (flag) {
+      errorActive.style.display = 'inline-block';
+    } else {
+      errorActive.style.display = 'none';
+    }
+  }
+}
+
+form.addEventListener('submit', (event) => {
+  for (let fillInput of inputs) {
+    if ((fillInput === pwd2) && (pwd.value !== pwd2.value)) {
+      showPasswordConfirmationError(fillInput, fillInput.nextElementSibling);
+      showAndHideErrorMessage(fillInput, true);
+      event.preventDefault();
+    } else if (!fillInput.validity.valid) {
+      showError(fillInput, fillInput.nextElementSibling);
+      showAndHideErrorMessage(fillInput, true);
+      event.preventDefault();
+      break;
+    }
+  };
+});
+
+function showError(inputValue, inputError) {
+  if (inputValue.validity.valueMissing) {
+    switch (inputValue.id) {
+      case 'name':
+        inputError.textContent = 'You need to enter your first name.';
+        break;
+
+      case 'last-name':
+        inputError.textContent = 'You need to enter your last name.';
+        break;
+
+      case 'email':
+        inputError.textContent = 'You need to enter an e-mail address.';
+        break;
+
+      case 'phone':
+        inputError.textContent = 'You need to enter your phone number.';
+        break;
+
+      case 'pwd':
+        inputError.textContent = 'You need to enter a password.';
+        break;
+
+      case 'pwd2':
+        inputError.textContent = 'You need to confirm your password.';
+        break;
+
+      default:
+        break;
+    }
+
+  } else if (inputValue.validity.typeMismatch) {
+    if (inputValue.id === 'email') {
+      inputError.textContent = 'Entered value needs to be an e-mail address.';
+    } else {
+      console.log(`Error: ${inputValue}`);
+    }
+
+  } else if (inputValue.validity.patternMismatch) {
+    if (inputValue.id === 'pwd') {
+      inputError.textContent = `Your password must be at least ${inputValue.minLength} characters as well as contain at least one uppercase, one lowercase, and one number.`;
+    } else {
+      console.log(`Error: ${inputValue}`);
+    }
+
+  } else if (inputValue.validity.tooShort) {
+    switch (inputValue.id) {
+      case 'name':
+        inputError.textContent = `First name should be at least ${inputValue.minLength} characters; you entered ${inputValue.value.length}.`;
+        break;
+
+      case 'last-name':
+        inputError.textContent = `Last name should be at least ${inputValue.minLength} characters; you entered ${inputValue.value.length}.`;
+        break;
+
+      case 'email':
+        inputError.textContent = `Your email should be at least ${inputValue.minLength} characters; you entered ${inputValue.value.length}.`;
+        break;
+
+      case 'phone':
+        inputError.textContent = `Your phone number should be at least ${inputValue.minLength} characters; you entered ${inputValue.value.length}.`;
+        break;
+
+      case 'pwd':
+        inputError.textContent = `Your password should be at least ${inputValue.minLength} characters; you entered ${inputValue.value.length}.`;
+        break;
+
+      default:
+        break;
+    }
+
+  } else if (inputValue.validity.tooLong) {
+    if (inputValue.id === 'pwd') {
+      inputError.textContent = `Your password should be at most ${inputValue.maxLength} characters; you entered ${inputValue.value.length}.`;;
+    }
+  } else if (inputValue.id === 'phone') {
+    inputError.textContent = 'Your phone number must be in this format: +123-123-123.';
+  }
+
+  inputError.className = 'error active';
+}
+
+function showPasswordConfirmationError(inputValue, inputError) {
+  if (pwd.value !== inputValue.value) {
+    inputError.textContent = `Your password confirmation is not correct.`;
+    inputError.className = 'error active';
+  }
+}
